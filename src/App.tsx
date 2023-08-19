945ba8ab6a79b60c64cb828c950a9988783de906
Hiko <hixohixo32@gmail.com>
2023-08-19 22:48:15 +0200

    configure tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return <main className="w-screen h-screen bg-background"></main>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "#f0f0f5",
+        background: "#020203",
+        primary: "#3d3d57",
+        secondary: "#0f0f15",
+        accent: "#62638d",
+      },
+    },
   },
   plugins: [],
 };
