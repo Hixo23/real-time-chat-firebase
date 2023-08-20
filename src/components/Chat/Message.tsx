1dbce2da03baca3335d1b93515edce036302e913
Hiko <hixohixo32@gmail.com>
2023-08-20 14:55:10 +0200

    Style: change diffrent background color on other user message

--- src/components/Chat/Message.tsx
+++ src/components/Chat/Message.tsx
@@ -22,8 +22,8 @@ export const Message = ({ content, displayName, photoURL }: IMessage) => {
         <span className="text-accent">{displayName}</span>
         <p
           className={`${
-            user?.displayName == displayName && "bg-blue-600"
-          } bg-green-600 py-2 md:px-6 px-3 rounded-xl `}
+            user?.displayName == displayName ? "bg-blue-600" : "bg-green-600"
+          }  py-2 md:px-6 px-3 rounded-xl `}
         >
           {content}
         </p>
