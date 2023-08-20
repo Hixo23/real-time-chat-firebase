915d39055900c9154c5917a5f25a40606c7ce08f
Hiko <hixohixo32@gmail.com>
2023-08-20 11:44:26 +0200

    Feat: add header component

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/index.css";
 
+initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
